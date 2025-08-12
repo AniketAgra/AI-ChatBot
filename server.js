47923158dba925e40d4ff85b6ba68c48fe58688b
Aniket Agrawal <ankt7987@gmail.com>
2025-08-13 02:02:03 +0530

    GEMINI INTEGRATION EMIT

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const app = require('./src/app');
 const {createServer} = require('http');
 const {Server} = require('socket.io');
+const generateResponse = require('./src/services/ai.service');
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
+//whenever a user connects, this will be called
 io.on("connection", (socket) => {
-  // ...
+  console.log("A user connected");
+
+  // Build-in event listeners for the socket
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+    socket.on("ai-message", async(data) => {
+        const response = await generateResponse(data.prompt);
+        console.log("AI Response:", response);
+        socket.emit("ai-response-message", { response });
+    });
 });
 
 httpServer.listen(3000, () => {
